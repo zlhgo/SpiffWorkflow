4aef33bf31985bdd8d2b2832db62ab52de481c0d
zlhgo <zlhgo@163.com>
2016-04-21 00:51:42 +0800

    modified shpinx-doc themes

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,8 @@ pygments_style = 'friendly'
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'sphinxdoc.css'
+#html_style = 'sphinxdoc.css'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -123,7 +124,7 @@ htmlhelp_basename = 'Sphinxdoc'
 latex_documents = [('contents', 'sphinx.tex', 'Sphinx Documentation',
                     'Georg Brandl', 'manual', 1)]
 
-latex_logo = '_static/sphinx.png'
+#latex_logo = '_static/sphinx.png'
 
 #latex_use_parts = True
 
